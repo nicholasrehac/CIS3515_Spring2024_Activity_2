f7dc81db23980ebf45d5e468fde053acd7ea6fc2
Nicholas Rehac <tup88289@temple.edu>
2024-02-06 12:13:19 -0500

    one-line numberArray initialization

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -14,10 +14,7 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 1: Populate this array */
-        val numberArray = ArrayList<Int>()
-        for(num in 2..100 step 2) {
-            numberArray.add(num)
-        }
+        val numberArray = (2..100 step 2).toList().toTypedArray()
 
         /* TODO Step 2: Create adapter to display items from array in Spinner */
         spinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
